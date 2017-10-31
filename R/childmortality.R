12775313d7bc7827b07d11fe7cb1da5624d79bfb
Casey Breen <caseyfbreen@umn.edu>
2017-10-31 10:31:41 -0500

    spooky commit

---
+++ R/childmortality.R
@@ -0,0 +1,166 @@
+########################################################
+# Master Function
+# 10/30/2017
+# Casey Breen
+########################################################
+
+#' Calculates childhood mortality
+#'
+#' calculates childhood mortality & SE's
+#'
+#' @param data dataframe object containing DHS variables YEAR, PSU, PERWEIGHT, KIDDOBCMC, INTDATECMC, KIDAGEDIEDIMP, and grouping var.
+#'
+#' @param grouping This character string gives the variable name of the variable denoting groups.
+#'
+#' @export
+childhoodmortality <- function(data, grouping, rate_type) {
+
+
+  # Set Default Rate Type
+  if(missing(rate_type)) {
+    rate_type <- "U5MR"
+  }
+
+
+  #generate master table
+  mortality_rates <- data.frame(a =c(), rate =c(), IFM = c())
+
+  data<- data[, c("YEAR", "GEOALT_NG2008_2013", "PSU", "PERWEIGHT", "KIDDOBCMC", "INTDATECMC", "KIDAGEDIEDIMP")]
+
+  age_segments <- list(c(0, 1),
+                       c(1, 2),
+                       c(3, 5),
+                       c(6, 11),
+                       c(12, 23),
+                       c(24, 35),
+                       c(36, 47),
+                       c(48, 59)
+  )
+
+  # What's happening here?
+  names(age_segments) <-
+    sapply(
+      age_segments,
+      function(x) {
+        paste0("age_", x[1], "_to_", x[2])
+      }
+    )
+
+  #Select observations for year 2008
+  group_levels <- unique(data[[grouping]])
+  for (group in group_levels) {
+    sub_sample <- data[which(data[[grouping]] == group),]
+
+    # Calculates Component death probabilities for each age interval
+    cdpw_sample <- compute_for_all_age_segments(sub_sample, age_segments)
+
+    # Calculates under-Five mortality rate from component death probablities
+
+    # Estimates based on rate type
+    if(rate_type == "infant") {
+      cdpw <- data.frame(cdpw=cdpw_sample$cdpw_sample[1:4])
+    } else if (rate_type == "neonatal") {
+      cdpw <- data.frame(cdpw=cdpw_sample$cdpw_sample[1:1])
+    } elsecdpw <- data.frame(cdpw=cdpw_sample$cdpw_sample)
+
+
+    # Call Utility Functions
+    mortality_type_rate <- calculate_component_survival_probabilities(cdpw)
+
+    append <- data.frame(group, mortality_type_rate)
+    mortality_rates <- rbind(mortality_rates, append)
+
+
+  }
+
+  ###############################################################
+  # Jack Knife Script for Standard Error Calculation
+  # Casey Breen
+  # 5/2/2017
+  #
+  # This portion of the script uses the Jackknife repeated replication
+  # method to derive estimates for standard errors for under-five
+  # mortality rates. This script replicates methods found in DHS
+  # Final Reports for estimating sampling error.
+  #################################################################
+
+  #Calculation of repeated replication of parent sample  ommiting obervations in "ith" PSU. Delete-one jackknife method used by DHS.
+
+  SE_rates <- data.frame(group =c(), SE =c())
+
+  #### update
+  for (group in group_levels) {
+    sub_sample <- data[which(data[[grouping]] == group),]
+    #Generate Vector
+    jack <- c()
+
+    #Find unique PSUs and create replications
+    PSU <- sub_sample$PSU
+    for (i in unique(PSU)) {
+
+      sub_sample_delete_i <-  sub_sample[which(!sub_sample$PSU == i),]
+
+      # Iterate over age segments
+      cdpw_sample <- compute_for_all_age_segments(sub_sample_delete_i, age_segments)
+
+      # Put all individual CDP values into dataframe
+
+      # Estimates based on rate type
+      if(rate_type == "neonatal") {
+        cdpw <- data.frame(cdpw=cdpw_sample$cdpw_sample[1:1])
+      } else if (rate_type == "infant") {
+        cdpw <- data.frame(cdpw=cdpw_sample$cdpw_sample[1:4])
+      } else cdpw <- data.frame(cdpw=cdpw_sample$cdpw_sample)
+
+
+      U5MR_rate <- calculate_component_survival_probabilities(cdpw)
+
+      jack <- append(jack, U5MR_rate)
+
+    }
+
+    #Jack Knife Calculation using formula found in DHS final reports
+
+    #Set values
+    r <- mean(jack)
+    k <- length(jack)
+
+    #Import vector as data frame
+    jack_frame <- data.frame('r_replication'=jack)
+    jack_frame$r_i <- (k*r)-(k-1)*(jack_frame$r_replication)
+
+    #Perform jack knife calculations
+    jack_frame$ri_r <- (jack_frame$r_i-r)^2
+    diff_sums <- jack_frame$ri_r
+    diff_sums2 <- sum(diff_sums)
+    SE <- sqrt ((1/(k*(k-1))*diff_sums2))
+
+    append <- data.frame(group, SE)
+
+    SE_rates <- rbind(SE_rates, append)
+
+  }
+
+  #################################################################################################################
+
+
+  #Merge U5 Mortality Rate with the U5 Standard Errors
+
+  disaggregate_mortality <- merge(mortality_rates, SE_rates, by="group", all=TRUE)
+
+
+  disaggregate_mortality <- plyr::rename(disaggregate_mortality, c("group" = grouping))
+  disaggregate_mortality$Lower_confidence_interval <- disaggregate_mortality$mortality_type_rate-2*disaggregate_mortality$SE
+  disaggregate_mortality$Upper_confidence_interval <- disaggregate_mortality$mortality_type_rate+2*disaggregate_mortality$SE
+
+  if(rate_type == "neonatal") {
+    disaggregate_mortality <- plyr::rename(disaggregate_mortality, c(mortality_type_rate = "neonatal"))
+  } else if (rate_type == "infant") {
+    disaggregate_mortality <- plyr::rename(disaggregate_mortality, c(mortality_type_rate = "infant"))
+  } else disaggregate_mortality <- plyr::rename(disaggregate_mortality, c(mortality_type_rate = "U5MR"))
+
+
+
+  return(disaggregate_mortality)
+}
+
--- R/utilities.R
+++ R/utilities.R
@@ -11,7 +11,7 @@ calculate_component_survival_probabilities <- function(df) {
 
   #Create calculate the product of all component survival probabilities
   cspw <- data.matrix(df$csp)
-  cspw_prod <- colProds(cspw)
+  cspw_prod <- matrixStats::colProds(cspw)
 
   #Subtract product from 1 and multiply by 1,000
   return(abs(cspw_prod - 1) * 1000)
@@ -22,16 +22,16 @@ compute_cdpw <- function(df, lower_age_segment, upper_age_segment) {
 
   #Numerator Calculation
   #Select sub-sample where age at death > lower age segment < upper age segment
-  T_num <- df[which(df$kidagediedimp >= lower_age_segment &
-                      df$kidagediedimp < upper_age_segment), ]
+  T_num <- df[which(df$KIDAGEDIEDIMP >= lower_age_segment &
+                      df$KIDAGEDIEDIMP < upper_age_segment), ]
 
   #Only calculate CDPs if dataset is populated
-  if (nrow(T_num) == 0) return(list(cdp = 0, cdp = 0))
+  if (nrow(T_num) == 0) return(list(cdp = 0, cdpw = 0))
 
   T_num <- compute_coweights(T_num, lower_age_segment, upper_age_segment)
 
-  T_den <- df[which(!df$kidagediedimp < (lower_age_segment + 1) |
-                      is.na(df$kidagediedimp)), ]
+  T_den <- df[which(!df$KIDAGEDIEDIMP < (lower_age_segment + 1) |
+                      is.na(df$KIDAGEDIEDIMP)), ]
   T_den <- compute_coweights(T_den, lower_age_segment, upper_age_segment)
 
   cdpw <- sum(T_num$coweight2, na.rm = TRUE) /
@@ -39,15 +39,19 @@ compute_cdpw <- function(df, lower_age_segment, upper_age_segment) {
   cdp <- sum(T_num$coweight, na.rm = TRUE) /
     (sum(T_den$coweight, na.rm = TRUE))
 
+  out <- (list(cdpw = cdpw, cdp = cdp))
+  if (!"cdpw" %in% names(out)) print (cdpw)
+
   return(list(cdpw = cdpw, cdp = cdp))
 }
 
+
 compute_for_all_age_segments <- function(df, age_segments) {
 
-  cdpw_2008 <- numeric(length = length(age_segments))
-  names(cdpw_2008) <- names(age_segments)
-  cdp_2008 <- numeric(length = length(age_segments))
-  names(cdp_2008) <- names(age_segments)
+  cdpw_sample <- numeric(length = length(age_segments))
+  names(cdpw_sample) <- names(age_segments)
+  cdp_sample <- numeric(length = length(age_segments))
+  names(cdp_sample) <- names(age_segments)
 
   for (i in seq_along (age_segments)) {
     age_seg <- age_segments[[i]]
@@ -58,11 +62,11 @@ compute_for_all_age_segments <- function(df, age_segments) {
 
     .cdpw <- compute_cdpw(df, lower_age_segment, upper_age_segment)
 
-    cdpw_2008[age_seg_label] <- .cdpw$cdpw
-    cdp_2008[age_seg_label] <- .cdpw$cdp
+    cdpw_sample[age_seg_label] <- .cdpw$cdpw
+    cdp_sample[age_seg_label] <- .cdpw$cdp
   }
 
-  return(list(cdpw_2008 = cdpw_2008, cdp_2008 = cdp_2008))
+  return(list(cdpw_sample = cdpw_sample, cdp_sample = cdp_sample))
 
 }
 
@@ -72,8 +76,8 @@ compute_coweights <- function(df, lower_age_segment, upper_age_segment) {
   df$au <- upper_age_segment
 
   #Set lower and upper limits of of time period
-  df$tu <- df$intdatecmc
-  df$tl <- df$intdatecmc - 60
+  df$tu <- df$INTDATECMC
+  df$tl <- df$INTDATECMC - 60
 
   #Calculate cohort limits
   df$tlau <- df$tl - df$au
@@ -82,13 +86,13 @@ compute_coweights <- function(df, lower_age_segment, upper_age_segment) {
   df$tual <- df$tu - df$al
 
   #Create the 3 cohorts by full exposure (1) or partial exposure (0.5)
-  df$coweight[df$kiddobcmc >= df$tlau & df$kiddobcmc < df$tlal] <- 0.5
-  df$coweight[df$kiddobcmc >= df$tlal & df$kiddobcmc < df$tuau] <- 1
-  df$coweight[df$kiddobcmc >= df$tuau & df$kiddobcmc < df$tual] <-
+  df$coweight[df$KIDDOBCMC >= df$tlau & df$KIDDOBCMC < df$tlal] <- 0.5
+  df$coweight[df$KIDDOBCMC >= df$tlal & df$KIDDOBCMC < df$tuau] <- 1
+  df$coweight[df$KIDDOBCMC >= df$tuau & df$KIDDOBCMC < df$tual] <-
     ifelse(upper_age_segment == 1, 1, 0.5)
 
   #Weight numerator by person weight
-  df$coweight2 <- df$coweight * df$perweight
+  df$coweight2 <- df$coweight * df$PERWEIGHT
 
   return(df)
 }
